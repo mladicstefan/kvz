8fff1bd4b2085aff2057da3d1a6765e042ba1df9
mladicstefan <stefan@mladicstefan.com>
2026-04-08 18:28:40 +0200

    persistence

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,14 @@
 const std = @import("std");
 const debug = std.debug.print;
 
-const data_dir_path = "/home/djamla/code/git/kvz/data/";
+const DATA_PATH = "/home/djamla/code/git/kvz/data/";
+const HEADER_OFFSET = 4;
 
 pub const Entry = struct {
     key_len: u32,
     value_len: u32,
-    key: []const u8,
-    value: []const u8,
+    key: []u8,
+    value: []u8,
 };
 
 const ParserError = error{
@@ -16,7 +17,7 @@ const ParserError = error{
     NotFound,
 };
 
-const KvError = std.fs.File.WriteError || std.fs.File.SeekError || std.mem.Allocator.Error || ParserError;
+const KvError = std.fs.File.WriteError || std.fs.File.SeekError || std.mem.Allocator.Error || ParserError || std.fs.File.ReadError;
 
 const Command = enum { GET, SET, DEL, LS };
 
@@ -48,18 +49,28 @@ fn parseInputs(allocator: std.mem.Allocator, line: []const u8, map: *std.StringH
                 if (res.found_existing) {
                     allocator.free(entry.key);
                     allocator.free(res.value_ptr.*);
-                }
-                res.value_ptr.* = entry.value;
-
-                try database.seekFromEnd(0);
-                inline for (std.meta.fields(Entry)) |field| {
-                    const value = @field(entry, field.name);
-                    if (field.type == []const u8) {
-                        try database.writeAll(value);
-                    } else {
-                        try database.writeAll(std.mem.asBytes(&value));
+                    res.value_ptr.* = entry.value;
+                } else {
+                    try database.seekTo(0);
+                    var bytes: [4]u8 = undefined;
+                    _ = try database.readAll(&bytes);
+                    var num: u32 = @bitCast(bytes);
+                    num += 1;
+                    try database.seekTo(0);
+                    try database.writeAll(&std.mem.toBytes(num));
+                    try database.seekFromEnd(0);
+                    res.value_ptr.* = entry.value;
+                    try database.seekFromEnd(0);
+                    inline for (std.meta.fields(Entry)) |field| {
+                        const value = @field(entry, field.name);
+                        if (field.type == []u8) {
+                            try database.writeAll(value);
+                        } else {
+                            try database.writeAll(std.mem.asBytes(&value));
+                        }
                     }
                 }
+
                 return entry;
             },
             .DEL => {
@@ -83,6 +94,32 @@ fn parseInputs(allocator: std.mem.Allocator, line: []const u8, map: *std.StringH
     return null;
 }
 
+fn sync(allocator: std.mem.Allocator, database: std.fs.File, map: *std.StringHashMap([]const u8), elems: u32) !void {
+    try database.seekTo(HEADER_OFFSET);
+    for (0..elems) |_| {
+        var entry: Entry = undefined;
+
+        var bytes: [4]u8 = undefined;
+        _ = try database.readAll(&bytes);
+        entry.key_len = @bitCast(bytes);
+        _ = try database.readAll(&bytes);
+        entry.value_len = @bitCast(bytes);
+
+        entry.key = try allocator.alloc(u8, entry.key_len);
+        entry.value = try allocator.alloc(u8, entry.value_len);
+        _ = try database.readAll(entry.key);
+        _ = try database.readAll(entry.value);
+
+        const res = try map.getOrPut(entry.key);
+
+        if (res.found_existing) {
+            allocator.free(entry.key);
+            allocator.free(res.value_ptr.*);
+        }
+        res.value_ptr.* = entry.value;
+    }
+}
+
 pub fn main() !void {
     var gpa: std.heap.DebugAllocator(.{}) = .init;
     defer _ = gpa.deinit();
@@ -100,11 +137,29 @@ pub fn main() !void {
     var reader = std.fs.File.stdin().reader(&read_buff);
     const stdin: *std.io.Reader = &reader.interface;
 
-    var data_dir = try std.fs.openDirAbsolute(data_dir_path, .{});
+    var data_dir = try std.fs.openDirAbsolute(DATA_PATH, .{});
     defer data_dir.close();
 
-    const database: std.fs.File = try data_dir.openFile("data.bin", .{ .mode = .read_write });
+    const database = data_dir.openFile("data.bin", .{ .mode = .read_write }) catch |err| switch (err) {
+        error.FileNotFound => blk: {
+            const f = try data_dir.createFile("data.bin", .{ .read = true });
+            try f.writeAll(&std.mem.toBytes(@as(u32, 0)));
+            try f.seekTo(0);
+            break :blk f;
+        },
+        else => return err,
+    };
+
     defer database.close();
+
+    var bytes: [4]u8 = undefined;
+    _ = try database.readAll(&bytes);
+    const num_elements: u32 = @bitCast(bytes);
+    try database.seekTo(HEADER_OFFSET);
+
+    debug("Syncing {d} elements...\n", .{num_elements});
+    try sync(allocator, database, &map, num_elements);
+    debug("Done, use responsibly (if possible)\n", .{});
 
     while (true) {
         const bare_line = try stdin.takeDelimiter('\n') orelse unreachable;
