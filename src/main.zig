e00c1c685b77dc6854b0b45f71fcd429d6826020
mladicstefan <stefan@mladicstefan.com>
2026-04-08 13:37:51 +0200

    set

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,88 @@
 const std = @import("std");
+const debug = std.debug.print;
 
-pub fn main() !void {
-    const data_dir_path = "/home/djamla/code/git/kvz/data/";
+const data_dir_path = "/home/djamla/code/git/kvz/data/";
 
+pub const Entry = struct {
+    key_len: u32,
+    value_len: u32,
+    key: []const u8,
+    value: []const u8,
+};
+
+const ParserError = error{
+    SyntaxError,
+    InvalidArguments,
+    NotFound,
+};
+
+const Command = enum { GET, SET, DEL };
+
+fn fileStuff() !void {
     var data_dir = try std.fs.openDirAbsolute(data_dir_path, .{});
     defer data_dir.close();
 
     const f: std.fs.File = try data_dir.openFile("data.bin", .{ .mode = .read_write });
     defer f.close();
+
     const res = try f.write("init");
     try f.seekTo(res);
     const bytes_written = try f.write(" Database");
 
     std.debug.print("{d}\n", .{bytes_written});
+}
+
+fn parseInputs(line: []const u8) ParserError!?Entry {
+    var iter = std.mem.tokenizeScalar(u8, line, ' ');
+    while (iter.next()) |curr| {
+        const cmd = std.meta.stringToEnum(Command, curr) orelse return ParserError.SyntaxError;
+        switch (cmd) {
+            .GET => {
+                debug("get", .{});
+            },
+            .SET => {
+                debug("set", .{});
+                const key = iter.next() orelse return ParserError.InvalidArguments;
+                const val = iter.next() orelse return ParserError.InvalidArguments;
+                return .{
+                    .key_len = @as(u32, @intCast(key.len)),
+                    .value_len = @as(u32, @intCast(val.len)),
+                    .key = key,
+                    .value = val,
+                };
+            },
+            .DEL => {
+                debug("del", .{});
+            },
+        }
+    }
+    return null;
+}
+
+pub fn main() !void {
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var map: std.StringHashMap([]const u8) = .init(allocator);
+    defer map.deinit();
+
+    var stdout_buf: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buf);
+    const stdout: *std.io.Writer = &stdout_writer.interface;
+
+    var read_buff: [1024]u8 = undefined;
+    var reader = std.fs.File.stdin().reader(&read_buff);
+    const stdin: *std.io.Reader = &reader.interface;
+
+    while (true) {
+        _ = try stdout.print("Enter a command: \n", .{});
+        try stdout.flush();
+
+        const bare_line = try stdin.takeDelimiter('\n') orelse unreachable;
+        const line = std.mem.trim(u8, bare_line, "\r");
+        if (try parseInputs(line)) |entry| {
+            debug("{any}", .{entry});
+        }
+    }
 }
